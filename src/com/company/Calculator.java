bbf6aac770f054ee6398e6907ef22278858e6dc1
Ekaterina Pak <ppakshome016@gmail.com>
2021-10-10 20:08:56 +0600

    without exs

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,28 +1,23 @@
 package com.company;
 
 
-import org.w3c.dom.ls.LSOutput;
 
 import java.util.function.*;
 
-public class Calculator {
+public class Calculator{
     static Supplier<Calculator> instance = Calculator::new;
 
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
     // Ошибка возникала из-за деления на 0
-    BinaryOperator<Integer> devide = (x, y) -> { try {
+    BinaryOperator<Integer> devide = (x, y) -> {
         // Обработать проверку деления на 0
-        if (y == 0)
-            throw new ArithmeticException("Exception: divide by zero.");
-        return x / y; // если b!=0, то вернуть результат деления
-    }
-    catch (ArithmeticException e) {
-        System.out.println(e.getMessage());
-        return 0;
-    }} ;
-
+        if (y == 0) {
+            System.out.println("/ by zero");
+        }
+        return x / y;
+    };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         int a = calc.plus.apply(45, 2);
         int b = calc.minus.apply(1,1);
-        int c = calc.devide.apply(5, 0);
+        int c = calc.devide.apply(5, 5);
 
         calc.println.accept(c);
         calc.println.accept(a);
